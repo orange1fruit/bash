17893f2fcd1d885b75051e9ea7259b9614c493b0
orange1fruit <roman@kovenya.ru>
2017-10-05 14:16:47 +0300

    v0.5

--- zimbra/tgz/backup-zimbra-tgz-daily-child.sh
+++ zimbra/tgz/backup-zimbra-tgz-daily-child.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+# variables
+mb=$1
+log=$2
+bp=$3
+# control of work
+cow ()
+{
+    log=$1
+    if [ $? -eq 0 ]; then
+    echo -e "\n[OK]\n" >> "$log"
+    else
+    echo -e "\n[FAIL]\n" >> "$log"
+    echo "The job backup-zimbra-tgz-daily was not executed. For more details, see the log" | /usr/bin/mutt -F /scripts/tgz/.muttrc -s "[Failed] The job backup-zimbra-tgz-daily was not executed" test@test.local -a $2
+    fi
+}
 # backup each mailbox
     echo "backup $mb" >> "$log"
     /opt/zimbra/bin/zmmailbox -z -m "$mb" getRestUrl "//?fmt=tgz" > "$bp/$mb.tgz"
-    cow
+    cow $log
     echo "complete backup $mb" >> "$log"
+
+# orangefruit v0.5
--- zimbra/tgz/backup-zimbra-tgz-daily.sh
+++ zimbra/tgz/backup-zimbra-tgz-daily.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
- 
-###################
 # script settings #
-###################
-# source
-source /scripts/tgz/cow
 # backup path
 bp="/mnt/backup/tgz/"
 # temp file
@@ -22,21 +17,29 @@ jcf ()
 {
     jcv=`ps ax -Ao ppid | grep $$ | wc -l`
 }
-###############
+# control of work
+cow ()
+{
+    if [ $? -eq 0 ]; then
+    echo -e "\n[OK]\n" >> "$log"
+    else
+    echo -e "\n[FAIL]\n" >> "$log"
+    echo "The job backup-zimbra-tgz-daily was not executed. For more details, see the log" | /usr/bin/mutt -F /scripts/tgz/.muttrc -s "[Failed] The job backup-zimbra-tgz-daily was not executed" r.kovenya@ruan.local -a $log
+    fi
+}
 # script body #
-###############
 echo "Start of job - $(date +%F) $(date +%T)" >> "$log"
 # begin time
 bt="$(date +%s)"
 # Create list closed & locked mailbox
 echo "Create list active mailbox" >> "$log"
-/opt/zimbra/bin/zmaccts | grep active | awk '{print $1}' | grep @ > "$tf"
+su zimbra -c /opt/zimbra/bin/zmaccts | grep active | awk '{print $1}' | grep @ > "$tf"
 cow
 # backup
 for mb in $( cat $tf)
 do
     jcf
-    /scripts/tgz/backup-zimbra-tgz-daily-child.sh $mb $log $bp
+    /scripts/tgz/backup-zimbra-tgz-daily-child.sh $mb $log $bp &
     while [ $jcv -ge $mc ]
     do
         jcf
@@ -49,12 +52,12 @@ echo "delete temp file" >> $log
 rm $tf
 cow
 # technical information
-uspbd=`/usr/bin/df -h | grep sdd | grep -o -E "[0-9]{2}"`
+uspbd=`df -h | grep sdd | grep -o -E "[0-9]{2}"`
 fspbd=$((100 - $uspbd))
-fsbd=`/usr/bin/df -h | grep sdd | awk '{print $4}'`
-uspsd=`/usr/bin/df -h | grep store | awk '{print $5}' | grep -o -E "[0-9]{2}"`
+fsbd=`df -h | grep sdd | awk '{print $4}'`
+uspsd=`df -h | grep store | awk '{print $5}' | grep -o -E "[0-9]{2}"`
 fspsd=$((100 - $uspsd))
-fssd=`/usr/bin/df -h | grep store | awk '{print $4}'
+fssd=`df -h | grep store | awk '{print $4}'
 # spent time
 end_time=`date +%s`
 elapsed_time=$(($end_time - $begin_time))
@@ -62,7 +65,7 @@ hours=$(($elapsed_time / 3600))
 elapsed_time=$(($elapsed_time - $hours * 3600))
 minutes=$(($elapsed_time / 60))
 seconds=$(($elapsed_time - $minutes * 60))
-total_time=`/usr/bin/echo "Spent time on job $hours hours  $minutes minutes  $seconds seconds" | tee -a $log
+total_time=`echo "Spent time on job $hours hours  $minutes minutes  $seconds seconds" | tee -a $log
 # send mail with log
 if [ $fspbd -lt 10 ]
 then
@@ -76,6 +79,6 @@ else
 body="The job backup-zimbra-tgz-daily is executed. For more details, see the log\n\nFree space on backup datastore - ${fsbd} / ${fspbd}%\n\nFree space on storage datastore - ${fssd} / ${fspsd}%\n\n${total_time}"  
 head="[Success] The job backup-zimbra-tgz-daily is executed"
 fi
-echo -e $body | /usr/bin/mutt -F /scripts/tgz/.muttrc -s "$head" v.pupkin@test.local -a $log $cron
+echo -e $body | /usr/bin/mutt -F /scripts/tgz/.muttrc -s "$head" r.kovenya@ruan.local -a $log $cron
 
-# orangefruit v0.2
+# orangefruit v0.4
